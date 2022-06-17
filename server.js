0dc7b339162e93634315538fe6980e3dff38c9f4
vanlinh00 <vanlinh20192019@gmail.com>
2022-06-18 00:43:43 +0700

    heroku

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
 
+
+
+
 const Player = require('./classes/Player');
-var players=[];
-var sockets=[];
-var io = require('socket.io')(4567);
-console.log('server has started');
+var players = [];
+var sockets = [];
 
-io.on('connection', function(socket)
-{
-    var newplayer =new Player();
-    var thisIdPlayer=newplayer.id;
-    players[thisIdPlayer]=newplayer;
-    sockets[thisIdPlayer]=socket;
-    socket.emit("dangki",{id:thisIdPlayer});
-    socket.emit("spawn",newplayer);
-    socket.broadcast.emit("spawn",newplayer);
 
-    for(var player in players)
-    {
-   console.log(player);
-       if(thisIdPlayer!=player)
-       {
-         socket.emit("spawn",players[player]);
-       }
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+
+  //console.log(socket);
+  var newplayer = new Player();
+  var thisIdPlayer = newplayer.id;
+  players[thisIdPlayer] = newplayer;
+  sockets[thisIdPlayer] = socket;
+  socket.emit("dangki", { id: thisIdPlayer });
+  socket.emit("newplayer", newplayer);
+  socket.broadcast.emit("newplayer", newplayer);
+
+
+  for (var player in players) {
+    if (thisIdPlayer != player) {
+      socket.emit("newplayer", players[player]);
     }
+  }
 
-    socket.on('updatePosition',function(data){
+  socket.on('updatePosition', function (data) {
     console.log(data);
-    newplayer.position.x= data.x;
-    newplayer.position.z=data.z;
-    socket.broadcast.emit('updatePosition',newplayer);
-    });
+    newplayer.position.x = data.x;
+    newplayer.position.z = data.z;
+    socket.broadcast.emit('updatePosition', newplayer);
+  });
 
 
-    socket.on('disconnect', function(reason){
+  socket.on('disconnect', function (reason) {
     console.log("da disconnect");
-     delete players[thisIdPlayer];
-     delete sockets[thisIdPlayer];
-     socket.broadcast.emit('disconnected',newplayer);
-    });
+    delete players[thisIdPlayer];
+    delete sockets[thisIdPlayer];
+    socket.broadcast.emit('disconnected', newplayer);
+  });
 
+});
+
+
+http.listen(4567, function(){
+  console.log('listening on *:4567');
 });
 
 
